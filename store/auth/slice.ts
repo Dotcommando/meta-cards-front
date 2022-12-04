5256f81df145c83dff80852c43424f0ea881d137
Mikhail Filchushkin <webestet@gmail.com>
2022-12-04 19:59:38 +0400

    Saving user on sign in added.

--- store/auth/slice.ts
+++ store/auth/slice.ts
@@ -53,105 +53,106 @@ const authSlice: Slice<IAuthState, SliceCaseReducers<IAuthState>> = createSlice(
     }),
   },
   extraReducers: (builder: ActionReducerMapBuilder<NoInfer<IAuthState>>) => {
-    builder.addCase(HYDRATE, (state: Draft<IAuthState>, action: Action<any>) => ({
-      ...state,
-      // @ts-ignore
-      ...action.payload.auth,
-    }));
-    builder.addCase(String(signUpReq.pending), (state: Draft<IAuthState>) => ({
-      ...state,
-      signUpUserIsLoading: true,
-      signUpUserIsSuccess: false,
-      signUpUserIsError: false,
-      signUpUserMessage: 'PENDING',
-      isAuthenticated: false,
-      isAdmin: false,
-      userId: null,
-      accessToken: '',
-      refreshToken: '',
-      accessTokenExpiredAfter: 0,
-      refreshTokenExpiredAfter: 0,
-    }));
-    builder.addCase(String(signUpReq.fulfilled), (state: Draft<IAuthState>, action: PayloadAction<IUserSignInRes>) => {
-      const { user, accessToken, refreshToken, accessTokenExpiredAfter, refreshTokenExpiredAfter } = action.payload;
+    builder
+      .addCase(HYDRATE, (state: Draft<IAuthState>, action: Action<any>) => ({
+        ...state,
+        // @ts-ignore
+        ...action.payload.auth,
+      }))
+      .addCase(String(signUpReq.pending), (state: Draft<IAuthState>) => ({
+        ...state,
+        signUpUserIsLoading: true,
+        signUpUserIsSuccess: false,
+        signUpUserIsError: false,
+        signUpUserMessage: 'PENDING',
+        isAuthenticated: false,
+        isAdmin: false,
+        userId: null,
+        accessToken: '',
+        refreshToken: '',
+        accessTokenExpiredAfter: 0,
+        refreshTokenExpiredAfter: 0,
+      }))
+      .addCase(String(signUpReq.fulfilled), (state: Draft<IAuthState>, action: PayloadAction<IUserSignInRes>) => {
+        const { user, accessToken, refreshToken, accessTokenExpiredAfter, refreshTokenExpiredAfter } = action.payload;
 
-      return {
+        return {
+          ...state,
+          signUpUserIsLoading: false,
+          signUpUserIsSuccess: false,
+          signUpUserIsError: false,
+          signUpUserMessage: '',
+          isAuthenticated: true,
+          isAdmin: user.role === ROLE.ADMIN,
+          userId: user._id,
+          accessToken,
+          refreshToken,
+          accessTokenExpiredAfter,
+          refreshTokenExpiredAfter,
+        };
+      })
+      .addCase(String(signUpReq.rejected), (state: Draft<IAuthState>, action: PayloadAction<any>) => ({
         ...state,
         signUpUserIsLoading: false,
         signUpUserIsSuccess: false,
-        signUpUserIsError: false,
-        signUpUserMessage: '',
-        isAuthenticated: true,
-        isAdmin: user.role === ROLE.ADMIN,
-        userId: user._id,
-        accessToken,
-        refreshToken,
-        accessTokenExpiredAfter,
-        refreshTokenExpiredAfter,
-      };
-    });
-    builder.addCase(String(signUpReq.rejected), (state: Draft<IAuthState>, action: PayloadAction<any>) => ({
-      ...state,
-      signUpUserIsLoading: false,
-      signUpUserIsSuccess: false,
-      signUpUserIsError: true,
-      // @ts-ignore
-      signUpUserMessage: action?.payload?.message ?? action?.error?.message ?? 'Cannot fetch data',
-      isAuthenticated: false,
-      isAdmin: false,
-      userId: null,
-      accessToken: '',
-      refreshToken: '',
-      accessTokenExpiredAfter: 0,
-      refreshTokenExpiredAfter: 0,
-    }));
-    builder.addCase(String(signInReq.pending), (state: Draft<IAuthState>) => ({
-      ...state,
-      signInUserIsLoading: true,
-      signInUserIsSuccess: false,
-      signInUserIsError: false,
-      signInUserMessage: 'PENDING',
-      isAuthenticated: false,
-      isAdmin: false,
-      userId: null,
-      accessToken: '',
-      refreshToken: '',
-      accessTokenExpiredAfter: 0,
-      refreshTokenExpiredAfter: 0,
-    }));
-    builder.addCase(String(signInReq.fulfilled), (state: Draft<IAuthState>, action: PayloadAction<IUserSignInRes>) => {
-      const { user, accessToken, refreshToken, accessTokenExpiredAfter, refreshTokenExpiredAfter } = action.payload;
+        signUpUserIsError: true,
+        // @ts-ignore
+        signUpUserMessage: action?.payload?.message ?? action?.error?.message ?? 'Cannot fetch data',
+        isAuthenticated: false,
+        isAdmin: false,
+        userId: null,
+        accessToken: '',
+        refreshToken: '',
+        accessTokenExpiredAfter: 0,
+        refreshTokenExpiredAfter: 0,
+      }))
+      .addCase(String(signInReq.pending), (state: Draft<IAuthState>) => ({
+        ...state,
+        signInUserIsLoading: true,
+        signInUserIsSuccess: false,
+        signInUserIsError: false,
+        signInUserMessage: 'PENDING',
+        isAuthenticated: false,
+        isAdmin: false,
+        userId: null,
+        accessToken: '',
+        refreshToken: '',
+        accessTokenExpiredAfter: 0,
+        refreshTokenExpiredAfter: 0,
+      }))
+      .addCase(String(signInReq.fulfilled), (state: Draft<IAuthState>, action: PayloadAction<IUserSignInRes>) => {
+        const { user, accessToken, refreshToken, accessTokenExpiredAfter, refreshTokenExpiredAfter } = action.payload;
 
-      return {
+        return {
+          ...state,
+          signInUserIsLoading: false,
+          signInUserIsSuccess: false,
+          signInUserIsError: false,
+          signInUserMessage: '',
+          isAuthenticated: true,
+          isAdmin: user.role === ROLE.ADMIN,
+          userId: user._id,
+          accessToken,
+          refreshToken,
+          accessTokenExpiredAfter,
+          refreshTokenExpiredAfter,
+        };
+      })
+      .addCase(String(signInReq.rejected), (state: Draft<IAuthState>, action: PayloadAction<any>) => ({
         ...state,
         signInUserIsLoading: false,
         signInUserIsSuccess: false,
-        signInUserIsError: false,
-        signInUserMessage: '',
-        isAuthenticated: true,
-        isAdmin: user.role === ROLE.ADMIN,
-        userId: user._id,
-        accessToken,
-        refreshToken,
-        accessTokenExpiredAfter,
-        refreshTokenExpiredAfter,
-      };
-    });
-    builder.addCase(String(signInReq.rejected), (state: Draft<IAuthState>, action: PayloadAction<any>) => ({
-      ...state,
-      signInUserIsLoading: false,
-      signInUserIsSuccess: false,
-      signInUserIsError: true,
-      // @ts-ignore
-      signInUserMessage: action?.payload?.message ?? action?.error?.message ?? 'Cannot fetch data',
-      isAuthenticated: false,
-      isAdmin: false,
-      userId: null,
-      accessToken: '',
-      refreshToken: '',
-      accessTokenExpiredAfter: 0,
-      refreshTokenExpiredAfter: 0,
-    }));
+        signInUserIsError: true,
+        // @ts-ignore
+        signInUserMessage: action?.payload?.message ?? action?.error?.message ?? 'Cannot fetch data',
+        isAuthenticated: false,
+        isAdmin: false,
+        userId: null,
+        accessToken: '',
+        refreshToken: '',
+        accessTokenExpiredAfter: 0,
+        refreshTokenExpiredAfter: 0,
+      }));
   },
 });
 
--- store/users/slice.ts
+++ store/users/slice.ts
@@ -1,8 +1,13 @@
 import { HYDRATE } from 'next-redux-wrapper';
 
+import { NoInfer } from 'react-redux';
+
 import {
+  Action,
+  ActionReducerMapBuilder,
   createEntityAdapter,
   createSlice,
+  Draft,
   EntityState,
   PayloadAction,
   Slice,
@@ -10,6 +15,8 @@ import {
 } from '@reduxjs/toolkit';
 
 import { IUser } from '../../common/types';
+import { IUserSignInRes, UsersState } from '../../types';
+import { signInReq, signUpReq } from '../auth';
 
 const usersAdapter = createEntityAdapter<IUser>({
   selectId: user => user._id,
@@ -26,13 +33,23 @@ const usersSlice: Slice<EntityState<IUser>, SliceCaseReducers<EntityState<IUser>
       usersAdapter.setAll(state, action.payload.users);
     },
   },
-  extraReducers: {
-    [HYDRATE]: (state, action) => {
-      return {
+  extraReducers: (builder: ActionReducerMapBuilder<NoInfer<EntityState<IUser>>>) => {
+    builder
+      .addCase(HYDRATE, (state: Draft<UsersState>, action: Action<any>) => ({
         ...state,
-        ...action.payload.users,
-      };
-    },
+        // @ts-ignore
+        ...action.payload.auth,
+      }))
+      .addCase(String(signUpReq.fulfilled), (state: Draft<UsersState>, action: PayloadAction<IUserSignInRes>) => {
+        const { user } = action.payload;
+
+        return usersAdapter.addOne(state, user);
+      })
+      .addCase(String(signInReq.fulfilled), (state: Draft<UsersState>, action: PayloadAction<IUserSignInRes>) => {
+        const { user } = action.payload;
+
+        return usersAdapter.addOne(state, user);
+      });
   },
 });
 
